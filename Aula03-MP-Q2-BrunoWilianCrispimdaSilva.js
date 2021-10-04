bd4771e2e94ea63062ab83f9bb4d9ea3ce1bd07f
Bruno Wilan <81261373+brunowilian@users.noreply.github.com>
2021-10-04 00:40:41 -0300

    Update Aula03-MP-Q2-BrunoWilianCrispimdaSilva.js

--- Aula03-MP-Q2-BrunoWilianCrispimdaSilva.js
+++ Aula03-MP-Q2-BrunoWilianCrispimdaSilva.js
@@ -10,11 +10,17 @@
  * Aula: Aula 03 - Variáveis, Tipos e Operadores - Mini-prova
  * Questão: 2
  *
+ 
+ O modelo de função exponencial é uma das formas mais simples de simular uma pandemia:
+
+Número de casos na semana = casos iniciais vezes (taxa de infecção elevada ao número da semana)
+
+Dado o modelo exponencial, e um número inicial de casos maior ou igual a um, implemente um programa que computa a quantidade de novos casos para as primeiras 10 semanas da pandemia.
+
  */
 
+
 var resultado = 0, iniciais = 1, infeccao = 2, semana = 10;
-
-
 
 for (var semana = 0; semana <10; semana++){
     resultado = iniciais * infeccao**semana;
